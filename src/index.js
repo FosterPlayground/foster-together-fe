e75df323bbd91db01698977002ff538e6a306bc4
jarrodskahill <jarrodskahill@gmail.com>
2020-02-27 22:50:44 -0800

    fixed analytics to display multiple routes

--- src/index.js
+++ src/index.js
@@ -8,17 +8,9 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 Sentry.init({
   dsn: 'https://4a0af58b3e8b4350984d11fe9a1f8cb0@sentry.io/2715170',
-})
-
-// const trackingId = 'UA-159166357-1'
-// ReactGA.initialize(trackingId, {
-//   debug: true,
-//   titleCase: false,
-//   gaOptions: {
-//     userId: 123,
-//     siteSpeedSampleRate: 100,
-//   },
-// })
+})(function initAnalytics() {
+  initGA('UA-159198142-1')
+})()
 
 ReactDOM.render(
   <Provider store={store}>
